73dfc680d1d8ed2d46828d8ae57058020f2dd250
Ana Júlia Amaro <126619273+AnaJAPR@users.noreply.github.com>
2024-05-20 22:14:33 -0300

    Update bubbleSort.h

--- bubbleSort/bubbleSort.h
+++ bubbleSort/bubbleSort.h
@@ -1,5 +1,13 @@
 #pragma once
 
+// Definição de estrutura para os nós da lista duplamente encadeada
+typedef struct Node
+{
+    int iPayload;   // Dados armazenados no nó
+    Node* ptrNext;  // Ponteiro para o próximo nó na lista
+    Node* ptrPrev;  // Ponteiro para o nó anterior na lista
+} Node;
+
 //Protótipos das funções
 Node* createNode(int);
 void displayList(Node*, ostream&);
