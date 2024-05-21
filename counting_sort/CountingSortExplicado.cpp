b618e2a1741b8c92e0d3309728f01a3ecf73ae0a
Eduarda Mesquita <126674559+eduardammag@users.noreply.github.com>
2024-05-21 16:47:28 -0300

    Update CountingSortExplicado.cpp

--- counting_sort/CountingSortExplicado.cpp
+++ counting_sort/CountingSortExplicado.cpp
@@ -2,38 +2,40 @@
 #include <vector>
 #include <algorithm>
 
+using namespace std;
+
 // Função para imprimir o estado atual do vetor
-void printArray(const std::vector<int>& arr, const std::string& msg) {
-    std::cout << msg << ": ";
+void printArray(const vector<int>& arr, const string& msg) {
+    cout << msg << ": ";
     for (int num : arr) {
-        std::cout << num << " ";
+        cout << num << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
-void countingSortVerbose(std::vector<int>& arr) {
+void countingSortVerbose(vector<int>& arr) {
     // Encontrar o maior valor no vetor
-    int max_val = *std::max_element(arr.begin(), arr.end());
-    std::cout << "Maior valor no vetor: " << max_val << std::endl;
+    int max_val = *max_element(arr.begin(), arr.end());
+    cout << "Maior valor no vetor: " << max_val << endl;
 
     // Criar um vetor de contagem com tamanho suficiente para armazenar contagens de 0 até max_val
-    std::vector<int> count(max_val + 1, 0);
+    vector<int> count(max_val + 1, 0);
     printArray(count, "Vetor de contagem inicial");
 
     // Contar a ocorrência de cada elemento no vetor original
     for (int num : arr) {
         count[num]++;
-        printArray(count, "Incrementando count[" + std::to_string(num) + "]");
+        printArray(count, "Incrementando count[" + to_string(num) + "]");
     }
 
     // Modificar o vetor de contagem para armazenar as posições dos elementos na saída
     for (size_t i = 1; i < count.size(); i++) {
         count[i] += count[i - 1];
-        printArray(count, "Atualizando count[" + std::to_string(i) + "]");
+        printArray(count, "Atualizando count[" + to_string(i) + "]");
     }
 
     // Criar um vetor de saída que será ordenado
-    std::vector<int> output(arr.size(), 0);
+    vector<int> output(arr.size(), 0);
     printArray(output, "Vetor de saída inicial");
 
     // Construir o vetor de saída
@@ -41,20 +43,20 @@ void countingSortVerbose(std::vector<int>& arr) {
         int num = arr[i];
         output[count[num] - 1] = num;
         count[num]--;
-        printArray(output, "Colocando " + std::to_string(num) + " na posição " + std::to_string(count[num]));
-        printArray(count, "Decrementando count[" + std::to_string(num) + "]");
+        printArray(output, "Colocando " + to_string(num) + " na posição " + to_string(count[num]));
+        printArray(count, "Decrementando count[" + to_string(num) + "]");
     }
 
     // Copiar o vetor de saída para o vetor original
     for (size_t i = 0; i < arr.size(); i++) {
         arr[i] = output[i];
-        printArray(arr, "Copiando de volta para arr[" + std::to_string(i) + "]");
+        printArray(arr, "Copiando de volta para arr[" + to_string(i) + "]");
     }
 }
 
 int main() {
     // Definindo o vetor original
-    std::vector<int> arr = {4, 2, 2, 8, 3, 3, 1};
+    vector<int> arr = {4, 2, 2, 8, 3, 3, 1};
     printArray(arr, "Vetor original");
     
     // Chamando a função de Counting Sort com verbose
