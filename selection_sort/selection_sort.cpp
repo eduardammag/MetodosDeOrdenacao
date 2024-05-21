48d2e852aa4c0142a04d0348ffcda324c606cfc0
Eduarda Mesquita <126674559+eduardammag@users.noreply.github.com>
2024-05-21 17:02:42 -0300

    Update selection_sort.cpp

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <cstdlib> //por vezes é necessário para o uso do malloc
 
-using std::cout;
-using std::endl;
+using namespace std;
 
 // Função para criar um novo nó com um inteiro fornecido
 Node* createNode(int iPayload)
